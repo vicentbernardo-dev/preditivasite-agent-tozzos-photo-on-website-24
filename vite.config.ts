04b093316789dab4e122075186f2a7cba3106e8b
Vicent Bernardo <vicent.bernardo@preditiva.co>
2026-09-21 13:41:46 -0300

    perf: elimina bloqueio de renderizacao com css inline e fontes assincronas

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
